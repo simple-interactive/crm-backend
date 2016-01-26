a6d74595a32bb54ef966f0fee59ff04143ad010c
Sveridov Denis <sveridovdenis@ukr.net>
2016-01-27 01:53:08 +0200

    fix

---
+++ application/models/Order.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @class App_Model_Order
+ *
+ * @property MongoId $id
+ * @property array   $data
+ * @property string  $status
+ * @property integer $createdDate
+ * @property string  $payStatus
+ * @property string  $tableId
+ * @property string  $userId
+ *
+ * @method static App_Model_Order[] fetchAll(array $cond = null, array $sort = null, $count = null, $offset = null, $hint = NULL)
+ * @method static App_Model_Order|null fetchOne(array $cond = null, array $sort = null)
+ * @method static App_Model_Order fetchObject(array $cond = null, array $sort = null)
+ */
+class App_Model_Order extends Mongostar_Model
+{
+
+}
--- application/modules/default/controllers/MdapiController.php
+++ application/modules/default/controllers/MdapiController.php
@@ -15,5 +15,46 @@ class MdapiController extends App_Controller_MDAuth
         $this->view->style = App_Map_Style::execute(
             $this->getMenuService()->getStyle($this->user)
         );
+        $products = App_Model_Product::fetchAll([
+            'userId' => (string) $this->user->id
+        ]);
+        $ids = [];
+        foreach ($products as $product) {
+            $ids [] = (string) $product->id;
+        }
+        $this->view->search = App_Map_Search::execute(App_Model_Search::fetchAll([
+            'productId' => ['$in' => $ids]
+        ]));
+    }
+
+    public function orderAction()
+    {
+        $orders = $this->getParam('orders', null);
+        if (empty($orders)) {
+            throw new \Exception('orders-invalid');
+        }
+        foreach ($orders as $item) {
+            $order = new App_Model_Order();
+            $order->id = new \MongoId($item['id']['$id']);
+            $order->data = $item['data'][0];
+            $order->status = $item['status'];
+            $order->createdDate = $item['createdDate'];
+            $order->payStatus = $item['payStatus'];
+            $order->tableId = $item['tableId'];
+            $order->userId = (string) $this->user->id;
+            $order->save();
+        }
+    }
+
+    public function productExistsAction()
+    {
+        $product = App_Model_Product::fetchOne([
+            'id' => $this->getParam('id', null)
+        ]);
+        if (!$product) {
+            throw new \Exception('Product not found', 400);
+        }
+        $product->exists = $this->getParam('exists', null);
+        $product->save();
     }
 } 
